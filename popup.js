3e07846c2087c3a306b7a754208667e253fb665a
haroonahmed204 <haroonahmed204@gmail.com>
2024-09-30 16:13:47 +0500

    Added Websites forms input and added dropdown button and radio button

--- popup.js
+++ popup.js
@@ -64,7 +64,7 @@ function attachEventListeners() {
   let autoFillBtn = document.getElementById("autoFill");
 
   // get text button
-  
+
   getTextBtn.addEventListener("click", function () {
     console.log("Fetch text button clicked");
   });
@@ -91,10 +91,10 @@ function fillForm() {
     first_name: "Haroon",
     last_name: "Ahmed",
     email: "haroon@example.com",
-    phone: "1234567890",
+    phone: "18182051090",
     company: "ABC",
-    city: "new york",
-    state: "New york",
+    city: "California",
+    state: "New York",
     state_ABv: "NY",
     zipcode: "10325",
     country: "United states",
@@ -102,7 +102,8 @@ function fillForm() {
     location: "New york, NY, USA",
     Address: "2393 Creekside Drive, Coplay, Pennsylvania, USA, 18037",
     password: "SecurePassword123!",
-    LinkedIn_url: "www.linkedin.com",
+    LinkedIn_url: "https://www.linkedin.com/in/haroon-ahmed-832594272/",
+    website: "www.website.com",
     cnic_number: "12345-6789101-1",
     date_of_birth: "09-28-2024",
     pronouns: "He/him",
@@ -114,69 +115,110 @@ function fillForm() {
     veteran_status: "No",
     diability: "no",
     How_did_you_hear_about_us: "Linkedin",
+    job_title: "account Executive",
+    job_location: "Mirpur ajk",
+    Description: "i was working on.....",
+    school_university: "Must Uni",
+    headline: "hey, i am.......",
+    salary: "50000",
   };
 
   function setInputValue(selector, value) {
     var input = document.querySelector(selector);
     if (input) {
-      input.value = value;
-      input.dispatchEvent(new Event("input", { bubbles: true }));
+      if (input.type === "radio") {
+        if (input.value === value) {
+          input.checked = true;
+          input.dispatchEvent(new Event("change", { bubbles: true }));
+        }
+      } else {
+        input.value = value;
+        input.dispatchEvent(new Event("input", { bubbles: true }));
+      }
     }
   }
 
+  setInputValue('input[name="cards[1bf0710b-bbcd-46b7-85a4-a6147be8ac64][field0]"]', 'I am located and authorized to work in the U.S, and do NOT live in NY, CA, or HI.')
+
   // set full name
   setInputValue(
-    'input[type="text"][name="name"]',
+    'input[type="text"][name="name"], input[type="text"][data-automation-id="legalNameSection_fullName"]',
     defaults.full_name
   );
 
   // set first name
   setInputValue(
-    'input[type="text"][name="firstname"], input[type=text][name="job_application[first_name]"]',
+    'input[type="text"][name="firstname"], input[type=text][name="job_application[first_name]"], input[type="text"][data-automation-id="legalNameSection_firstName"], input[aria-label="First Name"], input[name="firstName"], input[name="orV2-uLNL"]',
     defaults.first_name
+  );
+
+  // set Preferred name
+  setInputValue(
+    'input[aria-label="What is your preferred name?"]',
+    defaults.preferred_name
   );
 
   // set last name
   setInputValue(
-    'input[type="text"][name="lastname"], input[type=text][name="job_application[last_name]"',
+    'input[type="text"][name="lastname"], input[type=text][name="job_application[last_name]"], input[type="text"][data-automation-id="legalNameSection_lastName"], input[aria-label="Last Name"], input[name="lastName"], input[name="GTdQz2MZO3"]',
     defaults.last_name
   );
 
   // set email
   setInputValue(
-    'input[type="email"][name="email"], input[type="text"][autocomplete="email"]', 
+    'input[type="email"][name="email"], input[type="text"][autocomplete="email"], input[aria-label="Email"]',
     defaults.email
   );
 
+  setInputValue('input[type="text"][name="email"]', defaults.email);
+
   // set phone
   setInputValue(
-    'input[type="text"][name="phone"], input[type="text"][autocomplete="tel"],input[type="tel"][name="phone"]',
+    'input[type="text"][name="phone"], input[type="text"][autocomplete="tel"],input[type="tel"][name="phone"], input[type="text"][data-automation-id="phone-number"], input[aria-label="Phone"], input[name="phoneNumber"]',
     defaults.phone
   );
 
   // set company
   setInputValue(
-    'input[type="text"][name="org"]',
+    'input[type="text"][name="org"], input[type="text"][data-automation-id="company"], input[aria-label="Where have you most recently worked?"]',
     defaults.company
+  );
+
+  // set job title
+  setInputValue(
+    'input[type="text"][data-automation-id="jobTitle"]',
+    defaults.job_title
+  );
+
+  // set job location
+  setInputValue(
+    'input[type="text"][data-automation-id="location"]',
+    defaults.job_location
   );
 
   // set location
   setInputValue(
-    'input[type="text"][name="location"], input[type="text"][name="job_application[location]"]',
+    'input[type="text"][name="location"], input[type="text"][name="job_application[location]"], input[aria-label="Location"], input[name="q_cdf62a768c632854841cfa5e558cfe3c"], input[type="text"][name="address"]',
     defaults.location
   );
 
   // set linkedin url
   setInputValue(
-    'input[type="text"][name="urls[LinkedIn]"], input[autocomplete="custom-question-linkedin-profile"]',
+    'input[type="text"][name="urls[LinkedIn]"], input[autocomplete="custom-question-linkedin-profile"], input[type="text"][data-automation-id="linkedinQuestion"], input[aria-label="LinkedIn Profile"], input[type="text"][name="linkedinUrl"]',
     defaults.LinkedIn_url
   );
 
-  // set password
+  // set website
   setInputValue(
-    'input[type="password"][name="password"]',
-    defaults.password
+    'input[aria-label="Website"], input[type="text"][name="websiteUrl"]',
+    defaults.website
   );
+
+  // set headline
+  setInputValue('input[name="headline"]', defaults.headline);
+
+  // set password
+  setInputValue('input[type="password"][name="password"]', defaults.password);
 
   // set CNIC Number
   setInputValue(
@@ -190,6 +232,50 @@ function fillForm() {
     defaults.date_of_birth
   );
 
+  // set Address
+  setInputValue(
+    'input[type="text"][data-automation-id="addressSection_addressLine1"], Input[name="location.address"]',
+    defaults.Address
+  );
+
+  // set City
+  setInputValue(
+    'input[type="text"][data-automation-id="addressSection_city"], input[name="location.unknownLocation"], input[name="q_f9b680442a593a9db0199438ba40f1fe"], input[id="candidate-location"]',
+    defaults.city
+  );
+
+  // set select state
+  setInputValue('input[id="question_28331814002"]', defaults.state);
+
+  // set Zipcode
+  setInputValue(
+    'input[type="text"][data-automation-id="addressSection_postalCode"], input[name="location.postalCode"], input[name="q_1c26d5262c4fa424ce825510e8501b20"]',
+    defaults.zipcode
+  );
+
+  // set Description
+  setInputValue(
+    'input[type="text"][data-automation-id="description"]',
+    defaults.Description
+  );
+
+  // set School/University
+  setInputValue(
+    'input[type="text"][data-automation-id="school"]',
+    defaults.school_university
+  );
+
+  // set salary
+  setInputValue(
+    'input[name="q_14cb4cd95f61ba7ff368dd43567dc41a"], input[type="text"][data-ui="QA_8807074"]',
+    defaults.salary
+  );
+
+  // set pronouns
+  setInputValue(
+    'input[aria-label="What pronouns do you use?"]',
+    defaults.pronouns
+  );
 }
 
 function fetchDataFromDatabase() {
