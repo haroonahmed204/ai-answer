f68290af3506d19278270f619c3f0704f40599ab
Ahtasham Ilyas <ahtashamilyas2000@gmail.com>
2024-10-15 09:32:21 +0500

    test

---
+++ test.py
@@ -0,0 +1,11 @@
+from huggingface_hub import InferenceClient
+
+client = InferenceClient(api_key="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")
+
+for message in client.chat_completion(
+	model="mistralai/Mistral-7B-Instruct-v0.3",
+	messages=[{"role": "user", "content": "What is the capital of France?"}],
+	max_tokens=500,
+	stream=True,
+):
+    print(message.choices[0].delta.content, end="")
